110477016939b5b24fb8c8e71f53b3de14355c8f
Ariel Dupar <adupar@gmail.com>
2023-05-23 13:18:56 -0300

    AppInsight

--- python_webapp_flask/__init__.py
+++ python_webapp_flask/__init__.py
@@ -2,9 +2,35 @@
 The flask application package.
 """
 
-#from markupsafe import Markup
+# Code Added for Azure AppInsight
+from applicationinsights.flask.ext import AppInsights
+# End of code Azure AppInsight
 
 from flask import Flask
 app = Flask(__name__)
 
+# Code Added for Azure AppInsight
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = 'MY_INSTRUMENTATION_KEY'
+#'<YOUR INSTRUMENTATION KEY GOES HERE>'
+
+# log requests, traces and exceptions to the Application Insights service
+appinsights = AppInsights(app)
+# End of code Azure AppInsight
+
 import python_webapp_flask.views
+
+import os
+import logging
+# import log information on stdout
+from logging import StreamHandler
+# keep stdout/stderr logging using StreamHandler
+streamHandler = StreamHandler()
+app.logger.addHandler(streamHandler)
+# define log level to DEBUG
+app.logger.setLevel(logging.DEBUG)
+
+# force flushing application insights handler after each request
+@app.after_request
+def after_request(response):
+    appinsights.flush()
+    return response
--- python_webapp_flask/views.py
+++ python_webapp_flask/views.py
@@ -9,6 +9,12 @@ from python_webapp_flask import app
 @app.route('/')
 @app.route('/home')
 def home():
+    app.logger.debug('This is a debug log message')
+    app.logger.info('This is an information log message')
+    app.logger.warn('This is a warning log message')
+    app.logger.error('This is an error message')
+    app.logger.critical('This is a critical message')
+    
     """Renders the home page."""
     return render_template(
         'index.html',
@@ -35,3 +41,51 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+# Code Added for Azure AppInsight (track_custom_event)
+# Sending a custom event:
+@app.route('/track_custom_event')
+def track_custom_event():
+    from applicationinsights import TelemetryClient
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])
+    tc.track_event('ShoppingCartCheckout')
+    tc.flush()
+    return render_template('index.html')
+# End of code Azure AppInsight
+
+# Code Added for Azure AppInsight (send_metric)
+@app.route('/send_metric')
+def send_metric():
+    from applicationinsights import TelemetryClient
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])
+    import random
+    tc.track_metric('queueSize',  random.randint(0,5000))
+    tc.flush()
+    return render_template('index.html')
+# End of code Azure AppInsight
+
+# Code Added for Azure AppInsight (HandledException)
+# Logging an exception
+@app.route('/handledException')
+def handledException():
+    import sys
+    import string
+    import random
+    from applicationinsights import TelemetryClient
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])
+    
+    try:
+        randomstring = ''.join([random.choice(string.ascii_letters) for n in xrange(8)])
+        raise Exception("Exception {0}".format(randomstring))
+    except:
+       tc.track_exception()
+   
+    try:
+       randomstring = ''.join([random.choice(string.ascii_letters) for n in xrange(8)])
+       raise Exception("Exception {0}".format(randomstring))
+    except:
+       tc.track_exception(*sys.exc_info(), properties={ 'foo': 'bar' }, measurements={ 'x': 42 })
+       tc.flush()
+   
+    return render_template('index.html')
+# End of code Azure AppInsight
